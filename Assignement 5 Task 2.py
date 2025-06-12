b970ecf4d13e5cf7a452a73008dd25fc3db6048e
Sanketkate385 <sanket.kate385@gmail.com>
2025-06-12 22:51:59 +0530

    Add files via upload

---
+++ Assignement 5 Task 2.py
@@ -0,0 +1,6 @@
+list1 = [1,2,3,4,5,6,7,8,9,10]
+print("The Original List: ", list1)
+list2 =  list1[0:5]
+print("The Extracted First five element of the list: ",list2)
+list3 = list2[::-1]
+print("The Reverse of extracted element of the list: ",list3)
